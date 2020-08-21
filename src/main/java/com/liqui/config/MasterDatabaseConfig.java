a74bbf5868c5d6286c26171a68d288ac5b8aaef0
crack143 <allbook@gmail.com>
2020-08-22 05:05:05 +0530

    multiple data config test

--- src/main/java/com/liqui/config/MasterDatabaseConfig.java
+++ src/main/java/com/liqui/config/MasterDatabaseConfig.java
@@ -1,46 +1,57 @@
 package com.liqui.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-        entityManagerFactoryRef = "mysqlManagerFactory",
-        basePackages = {"com.mds"},
-        transactionManagerRef = "mysqlTransactionManager"
-)
-@PropertySource("classpath:application.properties")
+@PropertySource(value = { "classpath:application.properties"})
 public class MasterDatabaseConfig {
+
+	 @Profile("dev")
+	 @Bean(name = "devDataSource")
+	 @ConfigurationProperties(prefix = "dev.datasource")
+	 public DataSource devDataSource() {
+	  return DataSourceBuilder.create().build();
+	 }
 	
-	 @Bean(name = "mysqlDataSource")
-	 @ConfigurationProperties(prefix = "mysql.datasource")
-	 public DataSource dataSource() {
+	/*
+	 @Bean(name = "devJdbcTemplate") public JdbcTemplate
+	 devJdbcTemplate(@Qualifier(value="devDataSource") DataSource devDataSource) {
+	  return new JdbcTemplate(devDataSource); }
+	 */
+	 
+	 @Profile("prod")
+	 @Bean(name = "prodDataSource")
+	 @ConfigurationProperties(prefix = "prod.datasource")
+	 public DataSource prodDataSource() {
 	  return DataSourceBuilder.create().build();
 	 }
 
-	 @Bean(name = "mysqlJdbcTemplate")
-	 public JdbcTemplate MysqlJdbcTemplate(@Qualifier("mysqlDataSource") DataSource mysql) {
-	  return new JdbcTemplate(mysql);
+	/*
+	 * @Bean(name = "prodJdbcTemplate") public JdbcTemplate
+	 * prodJdbcTemplate(@Qualifier("prodDataSource") DataSource prodDataSource) {
+	 * return new JdbcTemplate(prodDataSource); }
+	 */
+	 @Profile("uat")
+	 @Bean(name = "uatDataSource")
+	 @ConfigurationProperties(prefix = "uat.datasource")
+	 public DataSource uatDataSource() {
+	  return DataSourceBuilder.create().build();
 	 }
+
+	/*
+	 * @Bean(name = "uatJdbcTemplate") public JdbcTemplate
+	 * uatJdbcTemplate(@Qualifier("uatDataSource") DataSource uat) { return new
+	 * JdbcTemplate(uat); }
+	 */
 	 
-	
 }
